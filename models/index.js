b1b0ab1037f73cd0facb1ab339a6947300210c8d
afrique18 <vincentv1225@gmail.com>
2023-07-18 16:08:40 -0400

    Added data models index file

---
+++ models/index.js
@@ -0,0 +1,35 @@
+const User = require('./User');
+const Blogpost = require('./Blogpost');
+
+
+User.hasMany(Blogpost, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Blogpost.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Blogpost.hasMany('Comment', {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+
+});
+
+Comment.belongsTo('Blogpost', {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+User.hasMany('Comment', {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+
+})
+
+Comment.belongsTo('User', {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+})
