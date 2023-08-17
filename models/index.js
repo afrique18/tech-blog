6aa29d5648e4133a2a9719660b0ac28bb0498a40
afrique18 <vincentv1225@gmail.com>
2023-08-17 05:26:40 -0400

    Modified index file for model

--- models/index.js
+++ models/index.js
@@ -1,38 +1,38 @@
-const User = require('./Post');
+const Post = require('./Post');
 const Blogpost = require('./Blogpost');
 const Comment = require('./Comment');
 
 
-User.hasMany(Blogpost, {
-    foreignKey: 'user_id',
+Blogpost.hasMany(Post, {
+    foreignKey: 'blogpost_id',
     onDelete: 'CASCADE'
 });
 
-Blogpost.belongsTo(User, {
-    foreignKey: 'user_id',
+Post.belongsTo(Blogpost, {
+    foreignKey: 'blogpost_id',
     onDelete: 'CASCADE'
 });
 
 Blogpost.hasMany(Comment, {
-    foreignKey: 'user_id',
+    foreignKey: 'blogpost_id',
     onDelete: 'CASCADE'
 
 });
 
 Comment.belongsTo(Blogpost, {
-    foreignKey: 'user_id',
+    foreignKey: 'blogpost_id',
     onDelete: 'CASCADE'
 });
 
-User.hasMany(Comment, {
-    foreignKey: 'user_id',
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
     onDelete: 'CASCADE'
 
 });
 
-Comment.belongsTo(User, {
-    foreignKey: 'user_id',
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
     onDelete: 'CASCADE'
 });
 
-module.exports = {User, Blogpost, Comment };
+module.exports = {Post, Blogpost, Comment };
